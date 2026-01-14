9b3c40b130979dce84e0aaef2ca8aeb8f05dc548
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2026-01-14 20:25:29 +0100

    chore: add implementations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ android {
 }
 
 dependencies {
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.5")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
