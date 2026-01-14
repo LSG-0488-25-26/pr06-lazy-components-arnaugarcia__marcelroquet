0f7c4bbad1f00c7c1a5561d01765080800e28465
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2026-01-14 22:48:00 +0100

    feat: implement countries details

--- app/src/main/java/com/example/layzydeverda/view/CountryDetailScreen.kt
+++ app/src/main/java/com/example/layzydeverda/view/CountryDetailScreen.kt
@@ -1,2 +1,99 @@
 package com.example.layzydeverda.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.navigation.NavController
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lazy_loading.viewModel.ApiViewModel
+
+@Composable
+fun CountryDetail(navController: NavController, viewModel: ApiViewModel, countryName: String,modifier: Modifier = Modifier){
+    val countries by viewModel.countries.observeAsState(emptyList())
+    val country = remember { countries.find { it.name == countryName } }
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            if (country != null) {
+                Image(
+                    painter = painterResource(id = country.flag),
+                    contentDescription = country.name,
+                    modifier = Modifier
+                        .size(200.dp)
+
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Text(
+                    text = country.name,
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    text = "Capital: ${country.capital}",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Medium,
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    text = "Població: ${country.population}",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Medium,
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+            } else {
+                Text(
+                    text = "País no trobat",
+                    fontSize = 22.sp,
+                    color = Color.Red,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            Button(
+                onClick = { navController.popBackStack() },
+                modifier = Modifier.padding(top = 20.dp)
+            ) {
+                Text("Tornar enrere", fontSize = 18.sp)
+            }
+        }
+    }
+}
+
+
+
